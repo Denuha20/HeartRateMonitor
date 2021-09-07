71b64be15251477271cb18eddcbe0b4e35ee128c
Denuha20 <73709021+Denuha20@users.noreply.github.com>
2021-09-07 10:52:09 +0100

    Update Database.java

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -6,6 +6,11 @@
 package Database;
 
 
+import Database.IDatabase;
+import OpenCOM.ILifeCycle;
+import OpenCOM.IMetaInterface;
+import OpenCOM.IUnknown;
+import OpenCOM.OpenCOMComponent;
 import java.io.*;
 
 /**
@@ -13,15 +18,45 @@ import java.io.*;
  * @author denuha
  */
 //store patient info
-public class Database {
-    private static int record;
+public class Database extends OpenCOMComponent implements IUnknown, IDatabase, IMetaInterface, ILifeCycle {
     
-    public Database(int a){
-        record = a;
+    static File file;
+    static FileWriter fw;
+    static BufferedWriter writer;
+
+    static void RecordDatabase(int a) throws IOException {
+        file = new File("/Users/denuha/NetBeansProjects/OpenComTest/src/main/java/kernel/RecordRates.txt");
+        fw = new FileWriter(file.getAbsoluteFile());
+        writer = new BufferedWriter(fw);
+        FileWriteHandler.writer("hello");
+        FileWriteHandler.writer(a);
+        FileWriteHandler.close();
     }
-    
-    public int getRecord(){
-        return record;
+    /** Creates a new instance
+     * @param pRuntime */
+    public Database(IUnknown pRuntime) {
+        super(pRuntime);
     }
 
+    /*static void in(int a) throws IOException {
+                
+    try {
+            FileWriter writer = new FileWriter("/Users/denuha/NetBeansProjects/OpenComTest/src/main/java/kernel/RecordRates.txt", true);
+            writer.write("Hello World");
+            writer.write("\r\n");   // write new line
+            writer.write(a);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     */
+    // ILifeCycle Interface
+    public boolean startup(Object pIOCM) {
+        return true;
+    }
+    
+    public boolean shutdown() {
+        return true;
+    }
 }
