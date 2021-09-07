1d99861e37d67ef329a06c0156fa6ed5b2d440ea
Denuha20 <73709021+Denuha20@users.noreply.github.com>
2021-09-07 10:51:27 +0100

    Add files via upload

---
+++ src/main/java/Database/IDatabase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import OpenCOM.IUnknown;
+
+/**
+ *
+ * @author denuha
+ */
+public interface IDatabase extends IUnknown{
+    
+    public void RecordDatabase(int x);
+}
